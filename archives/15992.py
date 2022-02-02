828eb2db32d68f9e81e5b45ff0e052a309b91286
jennyjunho2 <a01032208149@gmail.com>
2022-02-02 15:24:50 +0900

    220202

--- archives/15992.py
+++ archives/15992.py
@@ -15,7 +15,4 @@ for i in range(4, 1001):
 T = int(input())
 for _ in range(T):
 	n, m = map(int, input().split())
-	result = 0
-	for i in range(1, m+1):
-		result += dp[n][i]
-	print(result % mod)
+	print(dp[n][m])
