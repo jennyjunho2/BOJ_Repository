cc598673e130fef7dfa9ac699e7fec5cf3271b2c
jennyjunho2 <a01032208149@gmail.com>
2022-03-26 12:32:22 +0900

    .

--- answer_sheet.cpp
+++ answer_sheet.cpp
@@ -7,56 +7,13 @@
 using namespace std;
 using ll = long long;
 //-------------------------------------
-int cnt = 0;
-vector<string> split(string str, char delimiter) {
-    istringstream iss(str);
-    string buffer;
-    vector<string> result;
 
-    while (getline(iss, buffer, delimiter)) {
-        int buf_len = buffer.size();
-        if (buf_len) {
-            cnt++;
-            while (buf_len < 4) {
-                buffer = "0" + buffer;
-                buf_len++;
-            }
-        } else {
-            buffer = "-1";
-        }
-
-        result.push_back(buffer);
-    }
-
-    return result;
-}
+// https://www.acmicpc.net/workbook/view/10782
+// https://www.acmicpc.net/problem/20040
 
 int main() { fastio
 
-    string S; cin >> S;
-    vector<string> ip = split(S, ':');
-    vector<string> ans;
-    int ip_size = ip.size();
-    bool should_add = false;
-    FOR(i, 0, ip_size) {
-        string temp = ip[i];
-        if (temp == "-1") {
-            if (!should_add) {
-                int tmp = 8-cnt;
-                while (tmp--) {
-                    ans.push_back("0000");
-                }
-                should_add = true;
-            }
-        } else {
-            ans.push_back(temp);
-        }
-    }
-
-    FOR(i, 0, 8) {
-        cout << ans[i];
-        if (i < 7) { cout << ':';}
-    }
+    
 
     return 0;
 }
