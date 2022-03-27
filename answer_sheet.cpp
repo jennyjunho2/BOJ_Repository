397fc7708af5b9c4e1e78155d346925117272cb1
jennyjunho2 <a01032208149@gmail.com>
2022-03-27 20:29:45 +0900

    ..

--- answer_sheet.cpp
+++ answer_sheet.cpp
@@ -7,10 +7,43 @@
 using namespace std;
 using ll = long long;
 //-------------------------------------
+int N;
+int visited[10001];
+int nums[10001];
+queue<int> q;
 
 int main() {fastio
 
-    
+    cin >> N;
+    memset(visited, -1, sizeof(visited));
+    FOR(i, 1, N+1) { cin >> nums[i]; }
+    int a, b; cin >> a >> b;
+
+    q.push(a);
+    visited[a] = 0;
+
+    while(!q.empty()) {
+        int x = q.front();
+        int incr = nums[x];
+        int tmp = visited[x];
+        q.pop();
+
+        if (x == b) {
+            cout << tmp;
+            return 0;
+        }
+
+        while (true) {
+            x += incr;
+            if (x > b) { break; }
+            if (!(visited[x] == -1)) { continue; }
+
+            visited[x] = tmp + 1;
+            q.push(x);
+        }
+    }
+
+    cout << -1;
 
     return 0;
 }
