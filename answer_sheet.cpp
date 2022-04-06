ad2f0d555bc7b24d5a0deb15f16bde27a83067ba
jennyjunho2 <a01032208149@gmail.com>
2022-04-06 13:52:15 +0900

    220406

--- answer_sheet.cpp
+++ answer_sheet.cpp
@@ -7,40 +7,49 @@
 using namespace std;
 using ll = long long;
 //-------------------------------------
-// https://www.acmicpc.net/problem/7579
-int N, M;
-int memory[101];
-int cost[101];
-int dp[101][10001];
+// https://www.acmicpc.net/problem/2568
+using p = pair<int, int>;
+int N;
+vector<p> nums;
+int dp[100001];
+vector<int> v, LIS_exclude;
 
 int main() {fastio
 
-    cin >> N >> M;
-    FOR(i, 1, N+1) { cin >> memory[i]; }
-
-    int sum = 0;
-    FOR(i, 1, N+1) {
-        cin >> cost[i];
-        sum += cost[i];
+    cin >> N; nums.resize(N);
+    FOR(i, 0, N) {
+        cin >> nums[i].first >> nums[i].second;
     }
 
-    FOR(i, 1, N+1) {
-        int temp_memory = memory[i];
-        int temp_cost = cost[i];
-        FOR(j, 0, sum+1) {
-            if (j >= temp_cost) {
-                dp[i][j] = max(dp[i][j], dp[i-1][j-temp_cost] + temp_memory);
-            }
+    sort(nums.begin(), nums.end());
 
-            dp[i][j] = max(dp[i][j], dp[i-1][j]);
+    v.push_back(nums[0].second);
+    FOR(i, 1, N) {
+        int temp = nums[i].second;
+        if (temp > v.back()) {
+            v.push_back(temp);
+            dp[i] = v.size()-1;
+        } else {
+            auto it = lower_bound(v.begin(), v.end(), temp);
+            *it = temp;
+            dp[i] = it - v.begin();
         }
     }
 
-    FOR(i, 0, sum+1) {
-        if (dp[N][i] >= M) {
-            cout << i;
-            return 0;
+    int cnt = v.size();
+    cout << N - cnt << endl;
+    cnt--;
+    for (int i = N-1; i >= 0; i--) {
+        if (dp[i] == cnt) {
+            cnt--;
+            continue;
         }
+        LIS_exclude.push_back(nums[i].first);
+    }
+
+    int LIS_exclude_size = LIS_exclude.size();
+    FOR(i, 0, LIS_exclude_size) {
+        cout << LIS_exclude[i] << endl;
     }
 
     return 0;
